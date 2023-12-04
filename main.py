54f5ef251b09d3ffe8546dc018561daf391e00bb
Vanshika-j <vanshika.j@brudite.com>
2023-12-04 18:28:21 +0530

    resolve pep8 errors

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ Module to retrieve the latest news about Tesla from the News API.
 """
 import requests
 
+
 def get_latest_news():
     """
     Retrieve the latest news about Tesla from the News API.
 
-    This function makes a request to the News API to fetch the latest news articles
-    related to Tesla. It prints the title, description, and URL of each article.
+    This function makes a request to the News
+    API to fetch the latest news articles
+    related to Tesla. It prints the title,
+    description, and URL of each article.
     """
-    endpoint = ('https://newsapi.org/v2/everything?q=tesla'
-                '&from=2023-11-01&sortBy=publishedAt'
-                '&apiKey=0aade89dc52f498da7828b05f8cc6dc1')
+    endpoint = (
+        "https://newsapi.org/v2/everything?q=tesla"
+        "&from=2023-11-01&sortBy=publishedAt"
+        "&apiKey=0aade89dc52f498da7828b05f8cc6dc1"
+    )
 
     response = requests.get(endpoint, timeout=10)
     if response.status_code == 200:
-        articles = response.json()['articles']
+        articles = response.json()["articles"]
         for article in articles:
-            title = article['title']
-            description = article['description']
-            url = article['url']
-            print(f"Title: {title}\nDescription: {description}\nRead more: {url}\n")
+            title = article["title"]
+            description = article["description"]
+            url = article["url"]
+            print(f"Title: {title}\nDescription: {description}")
+
 
 get_latest_news()
